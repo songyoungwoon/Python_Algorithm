a7a2d21c6149d8caf019b7552bf96911f68fccf3
songyoungwoon <thdduddns98@naver.com>
2022-01-21 16:47:21 +0900

    greedy : 11000 : list fail

--- greedy/11000.py
+++ greedy/11000.py
@@ -1,34 +1,38 @@
 import sys
 from queue import PriorityQueue
+import time
 
 if __name__ == "__main__":
+    start = time.time()
     n = int(input())
-    lecture = PriorityQueue()
+    lecture = list()
     for i in range(n):
         s, t = map(int, sys.stdin.readline().split())
-        lecture.put((s, t))
-
+        lecture.append((s, t))
+    lecture.sort(key=lambda x:(x[1], x[0]))
     result = 0
-    temp_lecture = PriorityQueue()
-    while lecture.qsize() != 0:
+    temp_lecture = list()
+    while len(lecture) != 0:
         result += 1
-        last_finish_time = lecture.get()[1]
-        for i in range(lecture.qsize()):
-            s, t = lecture.get()
+        last_finish_time = lecture.pop(0)[1]
+        for i in range(len(lecture)):
+            s, t = lecture.pop(0)
             if s >= last_finish_time:
                 last_finish_time = t
             else:
-                temp_lecture.put((s, t))
-        if temp_lecture.qsize() != 0:
+                temp_lecture.append((s, t))
+        if len(temp_lecture) != 0:
             result += 1
-            last_finish_time = temp_lecture.get()[1]
-            for i in range(temp_lecture.qsize()):
-                s, t = temp_lecture.get()
+            last_finish_time = temp_lecture.pop(0)[1]
+            for i in range(len(temp_lecture)):
+                s, t = temp_lecture.pop(0)
                 if s >= last_finish_time:
                     last_finish_time = t
                 else:
-                    lecture.put((s, t))
+                    lecture.append((s, t))
     print(result)
+    end = time.time()
+    print(end - start)
 
 
 
