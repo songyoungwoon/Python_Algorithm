f369b81459b40091fa0fb134ee3fc45c9f20e28f
songyoungwoon <thdduddns98@naver.com>
2022-01-27 22:25:26 +0900

    dp : 1003 : for문

--- dp/1003.py
+++ dp/1003.py
@@ -1,28 +1,15 @@
-def fibo(n):
-    if n == 0:
-        dp0[n] = 1
-        return 0
-    elif n == 1:
-        dp1[n] = 1
-        return 1
-    else:
-        if dp[n] != -1:
-            return dp[n]
-        else:
-            dp[n] = fibo(n-1) + fibo(n-2)
-            dp0[n] = dp0[n-1] + dp0[n-2]
-            dp1[n] = dp1[n-1] + dp1[n-2]
-            return dp[n]
-
 if __name__ == "__main__":
     t = int(input())
     for i in range(t):
         n = int(input())
-        dp, dp0, dp1 = [-1]*(n+1), [0]*(n+1), [0]*(n+1)
+        dp0, dp1 = [0]*(n+1), [0]*(n+1)
         if n == 0:
-            dp[0] = 0
+            dp0[0] = 1
         else:
-            dp[0], dp[1] = 0, 1
-        fibo(n)
+            dp0[0], dp1[1] = 1, 1
+        for i in range(2, n+1):
+            dp0[i] = dp0[i - 1] + dp0[i - 2]
+            dp1[i] = dp1[i - 1] + dp1[i - 2]
+
         print(dp0[n], dp1[n])
 
