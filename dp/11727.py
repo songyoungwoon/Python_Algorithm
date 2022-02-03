6a46d1a371defc7330103eb886d811e446aa9e08
songyoungwoon <thdduddns98@naver.com>
2022-02-03 19:38:44 +0900

    dp : 11727

---
+++ dp/11727.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    n = int(input())
+
+    if n == 1:
+        print(1)
+        exit()
+
+    dp = [0 for _ in range(n + 1)]
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + 2*dp[i - 2]
+    print(dp[n]%10007)
