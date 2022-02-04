c08a5d18e5e61f7e5ab15abe6ab1b6485dc240b7
songyoungwoon <thdduddns98@naver.com>
2022-02-04 21:39:50 +0900

    dp : 11052

---
+++ dp/11052.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    n = int(input())
+    p = list(map(int, input().split()))
+
+    dp = [[0]*(n+1) for _ in range(n+1)]
+    dp[1][1] = p[0]
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i == 1 and j == 1:
+                continue
+            if j >= i:
+                dp[i][j] = max(dp[i - 1][j], p[i - 1] + dp[i][j - i])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    print(dp[n][n])
