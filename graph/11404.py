0cf4eafbb994a5425c4f8d1673ad5065fa69af5a
songyoungwoon <thdduddns98@naver.com>
2022-02-09 13:43:14 +0900

    graph : 11404 (Floyd)

--- graph/11404.py
+++ graph/11404.py
@@ -4,21 +4,29 @@ if __name__ == "__main__":
     n = int(input())
     m = int(input())
 
-    adj = [[sys.maxsize]*(n+1) for _ in range(n+1)]
+    adj = [[sys.maxsize]*(n) for _ in range(n)]
     for i in range(m):
         a, b, c = map(int, sys.stdin.readline().split())
+        a -= 1
+        b -= 1
         if adj[a][b] == 0:
             adj[a][b] = c
         else: adj[a][b] = min(adj[a][b], c)
-    for i in range(1, n+1):
-        for j in range(1, n+1):
+
+    for i in range(n):
+        for j in range(n):
             if i == j:
                 adj[i][j] = 0
 
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
                 adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j])
 
-    for i in range(1, len(adj)):
-        print(*adj[i][1:])
+    for i in range(n):
+        for j in range(n):
+            if adj[i][j] == sys.maxsize:
+                adj[i][j] = 0
+
+    for i in adj:
+        print(*i)
