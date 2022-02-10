438deb3f03d8d628c57f4e08467078568d0f3f58
songyoungwoon <thdduddns98@naver.com>
2022-02-10 13:14:05 +0900

    dp : 11051

---
+++ dp/11051.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    mod = 10007
+    n, k = map(int, input().split())
+    dp = [[0]*(k + 1) for _ in range(n + 1)]
+    for i in range(1, n+1):
+        for j in range(k+1):
+            if j == 0 or j == i:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+    print(dp[n][k] % mod)
