f1fd373459254fef4937716169c65d1985406ab2
songyoungwoon <thdduddns98@naver.com>
2022-02-13 21:33:47 +0900

    dbfs : 1697

--- dbfs/1697.py
+++ dbfs/1697.py
@@ -1,25 +1,20 @@
-from queue import Queue
+from collections import deque
 
 if __name__ == "__main__":
     n, k = map(int, input().split())
 
-    q = Queue()
-    q.put((0, n))
-    while not q.empty():
-        count, now = q.get()
-        if now < 0 or now > 100000:
+    q = deque()
+    q.append((0, n))
+    visit = [False]*100001
+    while q:
+        count, now = q.popleft()
+        if visit[now] == True:
             continue
+        visit[now] = True
         if now == k:
             print(count)
             exit()
-        q.put((count + 1, now * 2))
-        q.put((count + 1, now - 1))
-        q.put((count + 1, now + 1))
-
-
-
-
-
-
-
-
+        for i in [now*2, now - 1, now + 1]:
+            if i < 0 or i > 100000:
+                continue
+            q.append((count + 1, i))
